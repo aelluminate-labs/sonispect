91c93ca75698b546d22649ca0023b7054607b8a1
Francis Ignacio <ignacio.francis@aelluminate.com>
2024-11-21 07:12:40 +0800

    refactor: update accuracy method documentation to use inline comment style

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -3,8 +3,6 @@ from typing import List
 class Metrics:
     @staticmethod
     def accuracy(test_set: List, predictions: List) -> float:
-        """
-        Calculate accuracy of predictions.
-        """
+        # :: Calculate accuracy of predictions.
         correct = sum(1 for x in range(len(test_set)) if test_set[x][-1] == predictions[x])
         return correct / len(test_set)
